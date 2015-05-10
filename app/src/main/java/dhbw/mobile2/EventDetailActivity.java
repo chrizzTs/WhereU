ae0596ad8e6500938016ccf4131bb1ddda84bae5
Vincentxyz <vincentmzw@gmail.com>
2015-05-10 12:27:41 +0200

    Connection to Parse works

--- app/src/main/java/dhbw/mobile2/EventDetailActivity.java
+++ app/src/main/java/dhbw/mobile2/EventDetailActivity.java
@@ -1,5 +1,7 @@
 package dhbw.mobile2;
 
+import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,9 +11,14 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 
 public class EventDetailActivity extends ActionBarActivity {
@@ -25,6 +32,16 @@ public class EventDetailActivity extends ActionBarActivity {
             "Teilnehmer",
             "Teilnehmen/Abmelden",
             "Navigation starten"};
+    String category;
+    String description;
+    String duration;
+    int latitude;
+    String locationName;
+    int longitude;
+    int maxMembers;
+    String title;
+
+
 
 
     @Override
@@ -34,16 +51,7 @@ public class EventDetailActivity extends ActionBarActivity {
 
         Intent intent = getIntent();
 
-        detailListView = (ListView) findViewById(R.id.detailListView);
-        detailAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, eventDetails);
-        detailListView.setAdapter(detailAdapter);
-
-        detailListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getBaseContext(), parent.getItemIdAtPosition(position)+" is selected", Toast.LENGTH_LONG).show();
-            }
-        });
+        retrieveParseData();
     }
 
     @Override
@@ -68,12 +76,43 @@ public class EventDetailActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void createExampleEventData(){
-        ParseObject gameScore = new ParseObject("GameScore");
-        gameScore.put("score", 1337);
-        gameScore.put("playerName", "Sean Plott");
-        gameScore.put("cheatMode", false);
-        gameScore.saveInBackground();
+    public void retrieveParseData(){
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Event");
+        query.getInBackground("FDm6rc2uAI", new GetCallback<ParseObject>() {
+            public void done(ParseObject object, ParseException e) {
+                if (e == null) {
+                    category = object.getString("category");
+                    TextView detailCategoryDynamic = (TextView) (findViewById(R.id.detail_category_dynamic));
+                    detailCategoryDynamic.setText(category);
 
+                    description = object.getString("description");
+
+                    title = object.getString("title");
+                    TextView detailTitleDynamic = (TextView) (findViewById(R.id.detail_title_dynamic));
+                    detailTitleDynamic.setText(title);
+
+                    locationName = object.getString("locationName");
+                    longitude = object.getInt("longitude");
+                    latitude = object.getInt("latitude");
+                    duration = object.getString("duration");
+                    maxMembers = object.getInt("maxMembers");
+                } else {
+                    System.out.print("Object could not be received");
+                }
+            }
+        });
+    }
+
+    public void createExampleEventData(){
+        ParseObject event = new ParseObject("Event");
+        event.put("title", "Runde Flunkiball auf dem Campus");
+        event.put("description", "Wir wollten eine Runde Flunkiball auf dem Campus zocken. Dafür brauchen wir mindestens 10 Leute.");
+        event.put("category", "sport");
+        event.put("locationName", "Uni Mannheim");
+        event.put("longitude", 8.46181);
+        event.put("latitude", 49.483);
+        event.put("duration", "3 hours");
+        event.put("maxMembers", 30);
+        event.saveInBackground();
     }
 }
